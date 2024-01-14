2f335bd0c9ebd129493f05b46e4583bd775dbf59
rofifr <whoexaltsjune25@gmail.com>
2024-01-15 00:13:30 +0700

    declaration and expression

---
+++ function/func-declaration-dan-expression/functionDeclaration/functionDeclaration.js
@@ -0,0 +1,11 @@
+// function identifier (ParameterList opt) { FunctionBody }
+// ini adalah function yang dipelajari dari awal dengan penulisan atau urutan penulisan yang normal
+
+// contoh
+
+function cthFunction(cthSatu, cthDua) {
+  let hasil = cthSatu * cthDua;
+  console.log(hasil + " adalah hasil perkalian dari " + cthSatu + " dikali " + cthDua);
+  return;
+}
+cthFunction(20, 40);
---
+++ function/func-declaration-dan-expression/functionExpression/functionExpression.js
@@ -0,0 +1,10 @@
+// function identifier opt (ParameterList opt) { FunctionBody }
+// function Expression biasanya disimpan ke dalam sebuah variable
+// dan nama function atau identifiernya opsional
+
+// contoh
+
+let iniExpression = function (exNama) {
+  alert("Halo " + exNama);
+};
+iniExpression("Rofi");
