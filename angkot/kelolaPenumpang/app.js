82a832849525ba22677105b9baf2978b6ed00311
rofifr <whoexaltsjune25@gmail.com>
2024-01-21 23:11:35 +0700

    coba lagi

--- angkot/kelolaPenumpang/app.js
+++ angkot/kelolaPenumpang/app.js
@@ -1,7 +1,1 @@
 let penumpang = [];
-let tambahPenumpang = function (namaPenumpang, penumpang) {
-  if (penumpang == "") {
-    return tambahPenumpang("Rofi", penumpang);
-  }
-};
-console.log(penumpang);
