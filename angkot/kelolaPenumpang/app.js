9e3c5ece11a36c452488bc598a0de0af9751d7ce
rofifr <whoexaltsjune25@gmail.com>
2024-01-21 23:37:08 +0700

    nanti lagi, pusing lagi

--- angkot/kelolaPenumpang/app.js
+++ angkot/kelolaPenumpang/app.js
@@ -1,1 +1,19 @@
 let penumpang = [];
+let tambahPenumpang = function (namaPenumpang, penumpang) {
+  // jika angkot kosong
+  if (penumpang.length == 0) {
+    // tambah penumpang di awal array
+    penumpang.push(namaPenumpang);
+    return penumpang;
+  } else {
+    for (let indexPenumpang = 0; indexPenumpang < penumpang.length; indexPenumpang++) {
+      if (penumpang[indexPenumpang] == undefined) {
+        penumpang[indexPenumpang] = namaPenumpang;
+        return penumpang;
+      } else if (namaPenumpang == namaPenumpang) {
+        console.log("Penumpang sudah ada di dalam angkot");
+        return penumpang;
+      }
+    }
+  }
+};
