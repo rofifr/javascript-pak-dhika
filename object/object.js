a09e3538e0930fa877581ef76038de2a54321de5
rofifr <whoexaltsjune25@gmail.com>
2024-01-24 22:28:40 +0700

    tipe data object

---
+++ object/object.js
@@ -0,0 +1,16 @@
+// object tidak memiliki index, tapi object memiliki nama
+let myBio = {
+  myName: "Rofi Fathurrohman",
+  myAge: 22,
+  myAddress: {
+    street: "Jl. xyz, No. 123",
+    village: "Cwrk",
+    city: "Tasikmalaya",
+    province: "Jawa Barat",
+  },
+  myHobbies: {
+    fHobby: "Coding",
+    sHobby: "Editing",
+    tHobby: "Hang out",
+  },
+};
