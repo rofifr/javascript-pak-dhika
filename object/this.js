9a5376d6a9ac50b378f14223218a731aedf04943
rofifr <whoexaltsjune25@gmail.com>
2024-02-01 18:57:38 +0700

    evaluasi penulisan/pembuatan object

---
+++ object/this.js
@@ -0,0 +1,16 @@
+// evaluasi cara penulisan, penggunaan, pembuatan Object
+
+// Cara ke-1 - function declaration
+function exampleDeclaration() {
+  console.log("Hallo and Hai");
+}
+exampleDeclaration();
+
+// Cara ke-2 - Object literal
+let exampleLiteral = {};
+exampleLiteral.menyapa = function () {
+  console.log("Hallo and Hai");
+};
+exampleLiteral.menyapa();
+
+//
