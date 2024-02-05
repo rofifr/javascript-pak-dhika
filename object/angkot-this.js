a770687943fa8c0297d2359e8dd0f072e4f1067c
rofifr <whoexaltsjune25@gmail.com>
2024-02-05 10:56:43 +0700

    tambah angkot

--- object/angkot-this.js
+++ object/angkot-this.js
@@ -2,5 +2,10 @@
 
 // Menggunakan Constructor
 function MengelolaAngkot(sopir, trayek, penumpang, ongkos) {
-  (this.sopir = sopir), (this.trayek = trayek), (this.penumpang = penumpang), (this.ongkos = ongkos);
+  this.sopir = sopir;
+  this.trayek = trayek;
+  this.penumpang = penumpang;
+  this.ongkos = ongkos;
 }
+
+let angkotKeSatu = new MengelolaAngkot("Rofi Fathurrohman", ["Cibeureum", "Pancasila"], [], 0);
