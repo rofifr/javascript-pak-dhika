2ed707d778a4a2226c7900978f9618d473a40afb
rofifr <whoexaltsjune25@gmail.com>
2024-02-07 16:45:23 +0700

    document.querySelectorAll()

--- document-object-model/dom-selection/dom-selection.js
+++ document-object-model/dom-selection/dom-selection.js
@@ -27,12 +27,12 @@ for (let colorNum = 0; colorNum < paragraf.length; colorNum++) {
 const h1 = document.getElementsByTagName("h1")[0]; // jadi, karena ini array, maka indexnya harus tetap disertakan. index ini bisa disimpan di akhir seperti ini, atau dibuat ketika manipulasi akan dilakukan seperti sebelumnya
 h1.innerHTML = "Belajar DOM - Rofi";
 
-// ketiga, document.getElementsByClassName()
+// Ketiga, document.getElementsByClassName()
 const p1 = document.getElementsByClassName("p1"); // getElementsByClassName() -> juga mengembalikan HTMLCollections
 // dikarenakan getElementsByClassName() ini juga mengembalikan HTMLCollections dan menjadi sebuah array, maka cara untuk membuat perubahannya juga harus menyertakan indexnya, misal
 p1[0].innerHTML = "Ini adalah paragraf yang diubah menggunakan DOM Javascript"; // jadi penempatan penulisan indexnya bisa seperti ini (p1.[0]) atau diatas ketika pemanggilan atau ketika seleksi (const p1 = document.getElementsByClassName("p1")[0];)
 
-// keempat, document.querySellector()
+// Keempat, document.querySellector()
 const spanForSpan = document.querySelector("p.span1 span:nth-child(2)"); // querySelector() -> mengembalikan element
 spanForSpan.style.backgroundColor = "#2879ff";
 spanForSpan.style.color = "white";
@@ -41,3 +41,15 @@ spanForSpan.style.color = "white";
 // jika terdapat 4 span yang ada di dalam HTML, maka elemen span yang pertama yang akan diseleksi, contohnya di bawah ini
 const spanForSelect = document.querySelector("p.span1 span");
 spanForSelect.style.color = "orange"; // maka hanya akan mengubah warna element span yang pertama dia temukan
+
+// dengan itu, jika ingin semua span terseleksi maka harus menggunakan metode selector selanjutnya, yaitu
+// Kelima, document.querySelectorAll()
+const spanInSection = document.querySelectorAll("#section3 div span"); // querySelectorAll -> mengembalikan NodeList
+// namun, querySelectorAll() juga akan menjadi array, jadi ketika melakukan manipulasi atau perubahan harus menggunakan index, yang penempatan penulisan indexnya bisa seperti ini (spanInSection[0]) atau diatas ketika pemanggilan atau ketika seleksi (const spanInSection = document.querySelectorAll("#section3 div span")[0];)
+
+// spanInSection[0].style.backgroundColor = "yellow";
+
+// dan untuk bisa mengubah semuanya, kita bisa melakukannya seperti di awal, yaitu menggunakan looping
+for (let spanNum = 0; spanNum < spanInSection.length; spanNum++) {
+  spanInSection[spanNum].style.backgroundColor = "yellow";
+}
