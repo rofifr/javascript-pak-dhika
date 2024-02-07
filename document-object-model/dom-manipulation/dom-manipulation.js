6fe954fa0eed640ff59ce9b95d171f26515e5c3a
rofifr <whoexaltsjune25@gmail.com>
2024-02-07 23:11:08 +0700

    element.style<propertyCSS>

--- document-object-model/dom-manipulation/dom-manipulation.js
+++ document-object-model/dom-manipulation/dom-manipulation.js
@@ -9,16 +9,27 @@
 /* 
 
 • element.innerHTML
-• element.style.<property>
-• element.setAttribute( )
+• element.style.<propertyCSS>
+• element.setAttribute()
 • element.classList
 • ... (masih banyak lagi)
 */
 
-// innerHTML
-const judul = document.getElementById("title");
+// element.innerHTML
+const judul = document.getElementById("title1");
 // menggunakan innerHTML ini, dapat mengubah apapun isi di dalam element yang diseleksi
 judul.innerHTML = "Rofi <span style='color: blue'>Fathurrohman</span>";
-// atau
+// contoh lain
 const firstSection = document.querySelector("#section1 .p2");
 firstSection.innerHTML = "Ini adalah sebuah paragraf yang berisi <a href='#' style='background-color: #dedede'>link</a> di dalamnya";
+
+// element.style.<propertyCSS>
+const judulKedua = document.getElementsByTagName("h3")[0]; // getElementsByTagName() ini mengembalikan HTMLCollections maka harus menggunakan index
+judulKedua.style.backgroundColor = "yellow";
+judulKedua.style.color = "darkgoldenrod";
+judulKedua.innerHTML = "Text ini ditambahkan menggunakan DOM Javascript";
+
+// contoh lain
+const paragrafSection = document.querySelector("#section2 p");
+paragrafSection.style.fontWeight = "bold";
+paragrafSection.style.backgroundColor = "#bfd6ff";
