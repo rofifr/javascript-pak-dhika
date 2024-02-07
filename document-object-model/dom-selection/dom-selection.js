b51dc29c258fabc08344e0adb60b89e81e779187
rofifr <whoexaltsjune25@gmail.com>
2024-02-07 10:38:17 +0700

    getElementById()

---
+++ document-object-model/dom-selection/dom-selection.js
@@ -0,0 +1,6 @@
+// DOM Selection
+// penulisan code untuk menyeleksi
+
+// Pertama, document.getElementById()
+// document harus dituliskan terlebih dahulu, karena itu adalah root node dari apa yang akan kita seleksi
+const judul = document.getElementById("title");
